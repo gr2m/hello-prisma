bcb168ed25df2d7fb883d337426acf4b060316c8
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2023-08-19 10:50:16 -0700

    feat: add user script

---
+++ add-user.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+      posts: {
+        create: { title: "Hello World" },
+      },
+      profile: {
+        create: { bio: "I like turtles" },
+      },
+    },
+  });
+
+  console.log("user created");
+}
+
+try {
+  await main();
+} catch (error) {
+  console.error(error);
+} finally {
+  await prisma.$disconnect();
+}
