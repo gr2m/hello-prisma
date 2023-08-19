d28b0d92a2e2656c85807b26aa72651a5d099e56
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2023-08-19 10:53:04 -0700

    feat: list users with posts and profiles

--- list-users.ts
+++ list-users.ts
@@ -1,11 +1,18 @@
+import { inspect } from "node:util";
+
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const allUsers = await prisma.user.findMany();
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
 
-  console.log({ allUsers });
+  console.log(inspect(allUsers, { depth: Infinity }));
 }
 
 try {
